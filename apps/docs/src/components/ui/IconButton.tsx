e040dcf49abbb07b31460ad17e5a4f5840ff5054
leejh1316 <dlwogurtkfkd@gmail.com>
2026-02-11 18:00:48 +0900

    TODO 작성

--- apps/docs/src/components/ui/IconButton.tsx
+++ apps/docs/src/components/ui/IconButton.tsx
@@ -1,6 +1,7 @@
 import { Button as ButtonPrimitive } from "@timeless-ui/ui";
 import type { ButtonProps as ButtonPrimitiveProps } from "@timeless-ui/ui";
 import { forwardRef } from "react";
+import { DynamicIcon } from "lucide-react/dynamic";
 
 export type IconButtonSize = "xl" | "lg" | "md" | "sm" | "xs" | "tiny";
 interface IconButtonProps extends ButtonPrimitiveProps {
@@ -8,4 +9,5 @@ interface IconButtonProps extends ButtonPrimitiveProps {
 }
 const IconButton = forwardRef<React.ComponentRef<typeof ButtonPrimitive>, IconButtonProps>((props, forwardedRef) => {
   // return <ButtonPrimitive ref={forwardedRef} variant="ghost" {...props} />;
+  return <DynamicIcon name="" />;
 });
