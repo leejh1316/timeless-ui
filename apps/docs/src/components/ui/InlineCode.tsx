bf5882e2320a898cfc5174df97e3a1882eae833e
leejh1316 <dlwogurtkfkd@gmail.com>
2026-02-13 01:00:28 +0900

    feat: inline-code 스타일링용 속성 추가

--- apps/docs/src/components/ui/InlineCode.tsx
+++ apps/docs/src/components/ui/InlineCode.tsx
@@ -1,8 +1,14 @@
 import clsx from "clsx";
 import { ComponentProps } from "react";
 const InlineCode = ({ className, ...props }: ComponentProps<"code">) => {
-  return <code className={clsx(className, "font-code rounded-md bg-neutral-100 px-1.5 py-0.5 text-rose-600")} {...props} />;
+  return (
+    <code
+      data-inline-code
+      className={clsx(className, "font-code text-primary-500 rounded-md bg-neutral-100 px-1.5 py-0.5 tracking-normal")}
+      {...props}
+    />
+  );
 };
-InlineCode.displayName = "Code";
+InlineCode.displayName = "InlineCode";
 
 export { InlineCode };
