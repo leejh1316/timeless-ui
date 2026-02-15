8362abe0641821b1c9d8775922c8a983aa4c1d59
leejh1316 <dlwogurtkfkd@gmail.com>
2026-02-15 18:47:55 +0900

    feat: css table 컴포넌트 추가:

---
+++ apps/docs/src/components/common/CssPropertyTable.tsx
@@ -0,0 +1,44 @@
+import clsx from "clsx";
+import { forwardRef } from "react";
+import { InlineCode } from "../ui/InlineCode";
+import { Table } from "../ui/Table";
+
+type CssPropertyTableRow = {
+  name: string;
+  description: string;
+};
+interface CssPropertyTableProps extends React.ComponentProps<"table"> {
+  rows: CssPropertyTableRow[];
+}
+const CssPropertyTable = forwardRef<React.ComponentRef<"table">, CssPropertyTableProps>(({ rows, className, ...props }, forwardedRef) => {
+  return (
+    <Table.Root className={clsx(className)} ref={forwardedRef} {...props}>
+      <Table.Header>
+        <Table.Row>
+          <Table.Head>Prop</Table.Head>
+          <Table.Head>Description</Table.Head>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {rows.map(({ name, description }) => (
+          <Table.Row key={name}>
+            <Table.Cell>
+              <Table.CellLabel>Prop</Table.CellLabel>
+              <div className="flex items-center gap-1">
+                <InlineCode>{name}</InlineCode>
+              </div>
+            </Table.Cell>
+            <Table.Cell className="items-stretch">
+              <Table.CellLabel>Description</Table.CellLabel>
+              <div className="">{description}</div>
+            </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table.Root>
+  );
+});
+CssPropertyTable.displayName = "CssPropertyTable";
+
+export { CssPropertyTable };
+export type { CssPropertyTableProps, CssPropertyTableRow };
